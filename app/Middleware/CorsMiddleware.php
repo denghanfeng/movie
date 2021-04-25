e106436e6b1577a14ecb253a25cf068ac597aab0
denghanfeng <395153069@qq.com>
2021-04-25 18:27:03 +0800

    全局允许跨域

--- app/Middleware/CorsMiddleware.php
+++ app/Middleware/CorsMiddleware.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Middleware;
 
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Di\Annotation\Inject;
 use Hyperf\Utils\Context;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -12,14 +14,20 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class CorsMiddleware implements MiddlewareInterface
 {
+    /**
+     * @Inject()
+     * @var StdoutLoggerInterface
+     */
+    protected $logger;
+
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $response = Context::get(ResponseInterface::class);
         $response = $response->withHeader('Access-Control-Allow-Origin', '*')
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             // Headers 可以根据实际情况进行改写。
-            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,wx_id,openid');
-
+            ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,wx_id,openid,token');
+        $this->logger->alert(' CorsMiddleware '.__LINE__);
         Context::set(ResponseInterface::class, $response);
 
         if ($request->getMethod() == 'OPTIONS') {
